d3155a3c9ce2dcfba0ed7c5b73a7628fea5b0784
Valerio Narcisi <valerio.narcisi@gmail.com>
2024-01-15 10:35:12 +0100

    added another vertical image

--- app/routes/_index/Home.tsx
+++ app/routes/_index/Home.tsx
@@ -5,11 +5,35 @@ import Typography from "~/components/Typography/Typography";
 const Home: FC = () => {
   return (
     <Layout>
-      <img src="/images/example.jpg" alt="Example Image" />
+      <img src="/images/example-1.jpg" alt="Example Image" />
       <Typography variant="heading">Lorem ipsum dolor sit amet</Typography>
       <Typography variant="subheading">Lorem ipsum dolor sit amet</Typography>
-      <img src="/images/example-1.jpg" alt="Example Image" />
       <Typography variant="body">
+        <a href="#">Lorem ipsum dolor sit amet</a>, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco voluptate velit esse cillum dolore eu fugiat nulla pariatur laboris
+        nisi ut aliquip ex ea commodo consequat.
+        <br />
+        <br />
+        <a href="#">Lorem ipsum dolor sit amet</a>, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco voluptate velit esse cillum dolore eu fugiat nulla pariatur laboris
+        nisi ut aliquip ex ea commodo consequat.
+        <br />
+        <br />
+        <img src="/images/vertical-img.jpg" alt="Example Image" />{" "}
+        <a href="#">Lorem ipsum dolor sit amet</a>, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco voluptate velit esse cillum dolore eu fugiat nulla pariatur laboris
+        nisi ut aliquip ex ea commodo consequat.
+        <br />
+        <br />
+        <a href="#">Lorem ipsum dolor sit amet</a>, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco voluptate velit esse cillum dolore eu fugiat nulla pariatur laboris
+        nisi ut aliquip ex ea commodo consequat.
+        <br />
+        <br />
         <a href="#">Lorem ipsum dolor sit amet</a>, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
         exercitation ullamco voluptate velit esse cillum dolore eu fugiat nulla pariatur laboris
