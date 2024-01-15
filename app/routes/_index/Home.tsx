5eb3a983c444f8e1f541f383ed68a18f1da83da5
Valerio Narcisi <valerio.narcisi@gmail.com>
2024-01-15 10:29:34 +0100

    added some images

--- app/routes/_index/Home.tsx
+++ app/routes/_index/Home.tsx
@@ -5,36 +5,34 @@ import Typography from "~/components/Typography/Typography";
 const Home: FC = () => {
   return (
     <Layout>
+      <img src="/images/example.jpg" alt="Example Image" />
       <Typography variant="heading">Lorem ipsum dolor sit amet</Typography>
       <Typography variant="subheading">Lorem ipsum dolor sit amet</Typography>
+      <img src="/images/example-1.jpg" alt="Example Image" />
       <Typography variant="body">
         <a href="#">Lorem ipsum dolor sit amet</a>, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        <br /> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Typography>
-      <Typography variant="body">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        exercitation ullamco voluptate velit esse cillum dolore eu fugiat nulla pariatur laboris
+        nisi ut aliquip ex ea commodo consequat.
+        <br />
+        <br />
+        Duis aute irure dolor in reprehenderit in Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <iframe
+          width="100%"
+          height="500"
+          src="https://www.youtube.com/embed/7TpiIvEq5l8?si=bcs86orkqSdhOdb9"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <code>
+          console.log('aaaa') console.log('aaaa') console.log('aaaa') console.log('aaaa')
+          console.log('aaaa')
+        </code>
       </Typography>
       <button>Click ME</button>
-      <pre>
-        <code>{JSON.stringify({ foo: "bar" }, null, 2)}</code>
-      </pre>
-      <code>
-        @method_decorator(login_required, name='dispatch') class OrderSuccess(View): template_name =
-        'orders/order_success.html' form_class = UserAdditionalInfoForm # Common code for both get
-        and post reqs def dispatch(self, request, *args, **kwargs): order_id =
-        int(kwargs['order_id']) order = get_object_or_404(Order, pk=order_id) if order.user !=
-        request.user: raise Http404 self.order = order return super().dispatch(request, *args,
-        **kwargs) def get(self, request, order_id): user_additional_info_form = None if not
-        request.user.first_name: user_additional_info_form = self.form_class( instance=request.user)
-      </code>
     </Layout>
   );
 };
