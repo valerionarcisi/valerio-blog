27ae173d588e0afd3346650e0c18400378dfe7e9
Valerio Narcisi <valerio.narcisi@gmail.com>
2024-03-22 16:34:34 +0100

    functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@astrojs/react";
 
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
