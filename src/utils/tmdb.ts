7b672c5324974c9fddba03a12fcffbda41193766
Valerio Narcisi <valerio.narcisi@gmail.com>
2024-03-22 16:46:53 +0100

    clean up

--- src/utils/tmdb.ts
+++ src/utils/tmdb.ts
@@ -5,17 +5,9 @@ export const IMAGES_URL = 'https://image.tmdb.org/t/p/w500';
 export const BASE_URL = 'https://api.themoviedb.org/';
 export const VERSION = '3';
 
-export const fetchMovieById = (id: string): Promise<TMovieTmdb> => {
+export const fetchMovieById = async (id: string): Promise<TMovieTmdb> => {
     const url = `${BASE_URL}${VERSION}/movie/${id}?api_key=${API_KEY}`;
-
-    return fetch(url)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .catch((error) => {
-            console.error("There was a problem with your fetch operation:", error);
-        });
+    const response = await fetch(url);
+    const data = await response.json();
+    return data
 };
