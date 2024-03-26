0bcbc71ed6033cde62e9e2fffc1b3b09e386dcaf
Valerio Narcisi <valerio.narcisi@gmail.com>
2024-03-26 09:25:38 +0100

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from "astro/config";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
-import netlify from "@astrojs/netlify/functions";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   prefetch: true,
   output: "server",
   adapter: netlify(),
   vite: {
-    plugins: [vanillaExtractPlugin()],
-  },
+    plugins: [vanillaExtractPlugin()]
+  }
 });
