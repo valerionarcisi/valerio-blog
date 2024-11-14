75e65c837becb7971df0e84112df1b47882385d0
Valerio Narcisi <valerio.narcisi@gmail.com>
2024-11-14 18:15:57 +0100

    added configuration for CORS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://valerionarcisi.me',
+  site: "https://valerionarcisi.me",
   integrations: [mdx(), sitemap()],
+  server: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
 });
