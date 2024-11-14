97ee6a09c62fa9722b064f7e8e4411ebbb59092a
Valerio Narcisi <valerio.narcisi@gmail.com>
2024-11-14 22:05:19 +0100

    filterd last songs

--- src/services/audioscrobbler.ts
+++ src/services/audioscrobbler.ts
@@ -4,41 +4,44 @@ export const AUDIO_SCROBBLER_API_KEY = '35dcb09bbc9c0e8bee54210bace4ba66';
 export const AUDIO_SCROBBLER_USER = 'valerionar';
 export const AUDIO_SCROBBLER_VERSION = '2.0';
 
+const Track = zod.object({
+  name: zod.string(),
+  url: zod.string(),
+  date: zod
+    .object({
+      '#text': zod.string(),
+    })
+    .optional(),
+  album: zod.object({
+    '#text': zod.string(),
+  }),
+  artist: zod.object({
+    '#text': zod.string(),
+  }),
+  image: zod.array(
+    zod.object({
+      '#text': zod.string(),
+      size: zod.string(),
+    })
+  ),
+});
+
 const AudioScubblerSchema = zod.object({
   recenttracks: zod.object({
     track: zod.array(
-      zod.object({
-        name: zod.string(),
-        url: zod.string(),
-        date: zod
-          .object({
-            '#text': zod.string(),
-          })
-          .optional(),
-        album: zod.object({
-          '#text': zod.string(),
-        }),
-        artist: zod.object({
-          '#text': zod.string(),
-        }),
-        image: zod.array(
-          zod.object({
-            '#text': zod.string(),
-            size: zod.string(),
-          })
-        ),
-      })
+      Track
     ),
   }),
 });
 
 export type AudioScubblerResponse = zod.infer<typeof AudioScubblerSchema>;
+export type AudioScubblerTrack = zod.infer<typeof Track>;
 
 export const fetchRecentTracks = async (
   method = 'user.getrecenttracks',
   format = 'json',
   limit = '20'
-): Promise<AudioScubblerResponse> => {
+): Promise<AudioScubblerTrack[]> => {
   const params = new URLSearchParams({
     method,
     user: AUDIO_SCROBBLER_USER,
@@ -60,7 +63,9 @@ export const fetchRecentTracks = async (
     const jsonData = await response.json();
 
     // Validate the JSON response with Zod schema
-    const parsedData = AudioScubblerSchema.parse(jsonData);
+    const parsedData = AudioScubblerSchema.parse(jsonData).recenttracks.track.filter(
+      (track) => track.image[0]['#text'] !== 'https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png'
+    )
 
     return parsedData;
   } catch (error) {
